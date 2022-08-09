f12c3a39a893224fc0102ecc669d7eec03fa02ff
sara khalil <sarakhalil3377@gmail.com>
2022-08-09 11:42:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: "john",
+    address: "london",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newObj = { ...employee };
+    newObj[key] = value;
+    return newObj;
+
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, name) {
+    let newDeleteObj = { ...employee };
+    delete newDeleteObj.name
+    console.log(newDeleteObj);
+    return newDeleteObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+}
+
+
+
+
+
+/* scope
+updateEmployeeWithKeyAndValue(employee, key, value)
+
+var customerName = 'bob';
+var bestCustomer;
+
+const leastFavoriteCustomer = "james"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}*/
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not james";
+}
